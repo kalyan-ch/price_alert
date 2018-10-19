5ab2c1860055872f706517b05721b936b9524cc6
kalyan <kalyan.chaval123@gmail.com>
2018-10-18 19:19:22 -0600

    using a scheduler instead

---
+++ scheduler.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from price_alert import *
+
+def hourJob():
+	url = 'https://www.amazon.com/Gaming-i7-8750H-Display-Gigabit-FX504GM-ES74/dp/B07F6K21HJ/'
+	content = simple_get(url)
+    return get_price(content)
+
+scheduler = BlockingScheduler()
+scheduler.add_job(yayBoi, 'interval', minutes=5)
+scheduler.start()
+
+
